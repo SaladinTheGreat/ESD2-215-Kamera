779b344ff581f8aebe6dfb79791031fd3bf0ca6b
Constructor3000 <43009397+Constructor3000@users.noreply.github.com>
2024-05-02 15:10:48 +0200

    Add files via upload
    Nyere og bedre version

--- DirForMik.ino
+++ DirForMik.ino
@@ -19,7 +19,7 @@
   int MainPin;
 
 
-  int MikSampleSecondary[Asize];
+  int MikSampleSecond[Asize];
   int SecondPin;
     //De to arrays der bliver skrevet til for mik input, Hvilken pin er først skal findes senere.
 
@@ -34,14 +34,19 @@
   // put your setup code here, to run once:
   Serial.begin(115200);
 
+  Sneaker("Setting up",__LINE__);
 
-for (int i = 0; i < PinInSize; i++) {
+
+/*for (int i = 0; i < PinInSize; i++) {
     pinMode(PinIn[i], INPUT);
-}
-
+    Serial.println(PinIn[i]);
+}*/ 
+  pinMode(A0, INPUT);
+  pinMode(A2, INPUT);
 
 for (int i = 0; i < PinOutSize; i++) {
     pinMode(PinOut[i], OUTPUT);
+    Serial.println(PinOut[i]);
 }
 
 
@@ -56,6 +61,7 @@ void StartInterrupt() {
   for (int i = 0; i < PinInSize; i++){
   attachInterrupt(digitalPinToInterrupt(PinIn[i]), FirstSound, HIGH);
   }
+    Sneaker("setting interrupts 1st time",__LINE__);
     //Nulstiller alle input pin til ny lyd
 }
 
@@ -64,6 +70,7 @@ void ResetInterrupt () {
  for (int i = 0; i < PinInSize; i++){
   detachInterrupt(digitalPinToInterrupt(PinIn[i]));
   }
+   Sneaker("removing interrupts",__LINE__);
    //Detacher alle interupts på input pins.
 }
 
@@ -76,7 +83,7 @@ void DirectionInterrupt () {
   attachInterrupt(digitalPinToInterrupt(MainPin), MikTrig, HIGH);
   attachInterrupt(digitalPinToInterrupt(SecondPin), MikTrig, HIGH);
 
-
+    Sneaker("setting interrupts 2nd time",__LINE__);
 }
 
 
@@ -105,9 +112,11 @@ void FirstSound() {
     if (!FirstTrig) {
         // This block runs the first time an interrupt is triggered
         FirstTrig = true;
-        if (digitalRead(PinIn[0]) == HIGH) {
+        //if (digitalRead(PinIn[0]) == HIGH) {
+        if (digitalRead(A0) == HIGH) {
             MainPin = PinIn[0];
-        } else if (digitalRead(PinIn[1]) == HIGH) {
+        //} else if (digitalRead(PinIn[1]) == HIGH) {
+        } else if (digitalRead(A2) == HIGH) {
             MainPin = PinIn[1];
         } else {
             // Handle error: neither pin is HIGH when the interrupt is triggered
@@ -115,6 +124,7 @@ void FirstSound() {
             return;
         }
         detachInterrupt(digitalPinToInterrupt(MainPin)); // Disable further interrupts on the first triggered pin
+         Sneaker("setting main pin",__LINE__);
         return; // Exit after handling the first trigger
     }
 
@@ -122,15 +132,19 @@ void FirstSound() {
     if (FirstTrig) {
         // This block attempts to identify the second trigger
         FirstTrig = false; // Reset for potential future reactivation
-        if (digitalRead(PinIn[0]) == HIGH && PinIn[0] != MainPin) {
+        //if (digitalRead(PinIn[0]) == HIGH && PinIn[0] != MainPin) {
+        if (digitalRead(A0) == HIGH && PinIn[0] != MainPin) {
             SecondPin = PinIn[0];
-        } else if (digitalRead(PinIn[1]) == HIGH && PinIn[1] != MainPin) {
+        //} else if (digitalRead(PinIn[1]) == HIGH && PinIn[1] != MainPin) {
+        } else if (digitalRead(A2) == HIGH && PinIn[1] != MainPin) {
             SecondPin = PinIn[1];
         } else {
             // Handle error: second pin not found
             ErrorEx("Second Pin Not Found", __LINE__);
             return;
         }
+      Sneaker("setting second pin",__LINE__);
+
       ResetInterrupt(); // Reset interrupts
 
 
@@ -146,14 +160,30 @@ void MikTrig() {
   SampleToArray(MikSampleSecond, SecondPin);
   detachInterrupt(digitalPinToInterrupt(MainPin));
 
+  Sneaker("sampling to arrays",__LINE__);
 
-  CrossCorelate(MikSampleMain, MikSampleSecond, DelayXY);
+  for (int i=0; i<Asize;i++){
+    ;Serial.print(" || ");
+    Serial.print(MikSampleMain[i]);
+  }
+  Serial.print(" || ");
+
+
+  Serial.print("Mik Second Sample: ");
+    for (int i=0; i<Asize;i++){
+    Serial.print(" || ");
+    Serial.print(MikSampleSecond[i]);
+  }
+  Serial.print(" || ");
+  
+
+  DelayXY = CrossCorelate(MikSampleMain, MikSampleSecond);
 }
 
 
 
 
-CrossCorelate(int* Array1, int* Array2, int* Displace) {
+int CrossCorelate(int* Array1, int* Array2) {
   float CrossArray[Asize];
   int tArray [Asize];
   float dArray [Asize];
@@ -163,7 +193,7 @@ CrossCorelate(int* Array1, int* Array2, int* Displace) {
 
   for (int n=0; n <= Asize; n++) {
     for (int i=0; i <= Asize; i++) {
-      if (t+n <= Asize){
+      if (i+n <= Asize){
       tArray[i] = Array1[i] * Array2[i+n];
       } else {
         tArray[i] = 0;
@@ -181,7 +211,7 @@ CrossCorelate(int* Array1, int* Array2, int* Displace) {
 
   }
 
-  Max = dArray[0]
+  Max = dArray[0];
   Maxd = 0;
   for (int i=1; i <= Asize; i++){
     if(Max < dArray[i] ){
@@ -192,6 +222,8 @@ CrossCorelate(int* Array1, int* Array2, int* Displace) {
 
   }
 
+int delay = SampleTime * Maxd;
+  return delay;
 
 
 }
@@ -216,7 +248,16 @@ void SampleToArray(int* SamplingArray, int Pin) {
 
 
 
+void Sneaker(const char* SneakerMessage, int line) {
+    Serial.println(" ");
+    Serial.print("The Program is at line ");
+    Serial.println(line);
+    
+    Serial.print("The program is ");
+    
+    Serial.println(SneakerMessage);
 
+}
 
 
 
@@ -229,7 +270,6 @@ void ErrorEx(const char* errorMessage, int line) {
         delay(1000);  // Simple error loop to prevent further execution
     }
 }
-
 
 
 
